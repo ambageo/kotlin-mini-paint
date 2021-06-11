18553421f1578fc715aa425b1c321839e4971077
GeorgeAmpartzidis <george.ampartzidis@mpway.com>
2021-06-11 14:11:52 +0100

    set ContentView to the custom view created

--- app/src/main/java/com/example/minipaint/MainActivity.kt
+++ app/src/main/java/com/example/minipaint/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.minipaint
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val myCanvasView = MyCanvasView(this)
+
+        // This helps us use the entire screen
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+        } else {
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        }
+
+        myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
+        // Set the content view to out custom view
+        setContentView(myCanvasView)
     }
 }
