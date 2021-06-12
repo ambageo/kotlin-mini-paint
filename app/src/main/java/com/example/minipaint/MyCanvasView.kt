1fbd32698cd38da642302222ff36dffdca270a45
GeorgeAmpartzidis <george.ampartzidis@mpway.com>
2021-06-12 10:44:26 +0100

    Override onSizeChanged to create a new Canvas

--- app/src/main/java/com/example/minipaint/MyCanvasView.kt
+++ app/src/main/java/com/example/minipaint/MyCanvasView.kt
@@ -1,7 +1,28 @@
 package com.example.minipaint
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.util.Log
 import android.view.View
+import androidx.core.content.res.ResourcesCompat
 
 class MyCanvasView(context: Context) : View(context) {
+
+    private lateinit var extraCanvas: Canvas
+    private lateinit var extraBitmap: Bitmap
+
+    private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+
+    override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
+        super.onSizeChanged(width, height, oldWidth, oldHeight)
+Log.d("ggg", "onSizeChanged")
+        // Every time the function executes, a new bitmap and canvas are created.
+        // To avoid memory leaks, we need to recycle extraBitmap before creating a new one
+        if (::extraBitmap.isInitialized) extraBitmap.recycle()
+        extraBitmap = Bitmap.createBitmap(width, height,Bitmap.Config.ARGB_8888)
+        extraCanvas = Canvas(extraBitmap)
+        extraCanvas.drawColor(backgroundColor)
+
+    }
 }
